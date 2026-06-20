fe5b32da9e8f2d86646ea8e1f6d89216c36b444a
moraissdev <renanbusinessdev@gmail.com>
2026-06-20 12:56:05 -0300

    style: adding emojis

--- frontend/my-app/src/componentes/Footer/Footer.tsx
+++ frontend/my-app/src/componentes/Footer/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer(){
     return(
         <footer className='footer'>
             <section>
-                <p style={{textAlign: "center"}}>Feito por <a href="https://github.com/renanmoraiss" target='_blank'>Renan Morais</a> e <a href="https://github.com/sabjoao" target='_blank'>João Pedro Sabino</a></p>
+                <p style={{textAlign: "center"}}>Feito por <a href="https://github.com/renanmoraiss" target='_blank'>Renan Morais</a> e <a href="https://github.com/sabjoao" target='_blank'>João Pedro Sabino</a>🧑‍💻</p>
             </section>
         </footer>
     );
